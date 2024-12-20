a4c25d507925618fd77b61d972b79da55857231e
Vladislav Efremenko <vlad_jef@rambler.ru>
2024-12-20 16:46:20 +0400

    Task 3 is done

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,24 +1,30 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
+resource "yandex_vpc_subnet" "vm_web_develop" {
+  name           = var.vm_web_vpc_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
-
+resource "yandex_vpc_subnet" "vm_db_develop" {
+  name           = var.vm_db_vpc_name
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.vm_db_default_cidr
+}
 
 data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2004-lts"
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = var.yandex_compute_instance_name
+  platform_id = var.yandex_compute_instance_platform_id
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.yandex_compute_instance_cores
+    memory        = var.yandex_compute_instance_memory
+    core_fraction = var.yandex_compute_instance_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -29,7 +35,36 @@ resource "yandex_compute_instance" "platform" {
     preemptible = true
   }
   network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
+    subnet_id = yandex_vpc_subnet.vm_web_develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_yandex_compute_instance_name
+  platform_id = var.vm_db_yandex_compute_instance_platform_id
+  zone        = var.vm_db_zone
+  resources {
+    cores         = var.vm_db_yandex_compute_instance_cores
+    memory        = var.vm_db_yandex_compute_instance_memory
+    core_fraction = var.vm_db_yandex_compute_instance_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.vm_db_develop.id
     nat       = true
   }
 
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,38 +1,66 @@
-###cloud vars
-
-
-variable "cloud_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
-}
-
-variable "folder_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
-}
-
-variable "default_zone" {
-  type        = string
-  default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
-}
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-}
-
-variable "vpc_name" {
-  type        = string
-  default     = "develop"
-  description = "VPC network & subnet name"
-}
-
-
-###ssh vars
-
-variable "vms_ssh_root_key" {
-  type        = string
-  default     = "<your_ssh_ed25519_key>"
-  description = "ssh-keygen -t ed25519"
-}
+# ###cloud vars
+#
+#
+# variable "cloud_id" {
+#   type        = string
+#   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+# }
+#
+# variable "folder_id" {
+#   type        = string
+#   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+# }
+#
+# variable "default_zone" {
+#   type        = string
+#   default     = "ru-central1-a"
+#   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+# }
+# variable "default_cidr" {
+#   type        = list(string)
+#   default     = ["10.0.1.0/24"]
+#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+# }
+#
+# variable "vpc_name" {
+#   type        = string
+#   default     = "develop"
+#   description = "VPC network & subnet name"
+# }
+#
+#
+# ###ssh vars
+#
+# variable "vms_ssh_root_key" {
+#   type        = string
+#   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/l7aD5x+bX9c0L7odxlFD1wTreEcXJDZ/Cb/aKFj3E admin@ubuntu-50"
+#   description = "ssh-keygen -t ed25519"
+# }
+#
+# variable "yandex_compute_instance_name" {
+#   type        = string
+#   default     = "netology-develop-platform-web"
+#   description = "yandex_compute_instance name"
+# }
+#
+# variable "yandex_compute_instance_platform_id" {
+#   type        = string
+#   default     = "standard-v1"
+#   description = "standard-v1"
+# }
+#
+# variable "yandex_compute_instance_cores" {
+#   type        = number
+#   default     = 2
+#   description = "2"
+# }
+# variable "yandex_compute_instance_memory" {
+#   type        = number
+#   default     = 1
+#   description = "1"
+# }
+# variable "yandex_compute_instance_core_fraction" {
+#   type        = number
+#   default     = 5
+#   description = "5"
+# }
